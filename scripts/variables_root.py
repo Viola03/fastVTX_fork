cfb478200a0b27c426b1cb74acb317016a0ce2bf
Alexander Mclean Marshall <alex.marshall@cern.ch>
2024-05-22 17:04:31 +0100

    begin root

---
+++ scripts/variables_root.py
@@ -0,0 +1,121 @@
+from fast_vertex_quality.tools.config import read_definition, rd
+
+import fast_vertex_quality.tools.data_loader as data_loader
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import vector
+import fast_vertex_quality.tools.variables_tools as vt
+import uproot
+
+masses = {}
+masses[321] = 493.677
+masses[211] = 139.57039
+masses[13] = 105.66
+masses[11] = 0.51099895000 * 1e-3
+
+
+
+print("Opening file...")
+
+file = uproot.open("/users/am13743/fast_vertexing_variables/datasets/B2KEE_three_body_cut.root:DecayTree")
+# events = file.arrays(library='pd', entry_stop=10000)
+events = file.arrays(library='pd')
+print("Opened file as pd array")
+print(events.shape)
+
+pid_list = [11,13,211,321]
+
+particles = ["A", "B", "C"]
+
+for particle in particles:
+    events = events[np.abs(events[f'{particle}_TRUEID']).isin(pid_list)]
+
+for particle in particles:
+    mass = np.asarray(events[f'{particle}_TRUEID']).astype('float32')
+    for pid in pid_list:
+        mass[np.where(np.abs(mass)==pid)] = masses[pid]
+    events[f'{particle}_mass'] = mass
+
+print(events.shape)
+
+for particle_i in range(0, len(particles)):
+    for particle_j in range(particle_i + 1, len(particles)):
+        (
+            events[f"m_{particle_i}{particle_j}"],
+            events[f"m_{particle_i}{particle_j}_inside"],
+        ) = vt.compute_mass(
+            events,
+            particles[particle_i],
+            particles[particle_j],
+            events[f'{particles[particle_i]}_mass'],
+            events[f'{particles[particle_j]}_mass'],
+        )
+
+events[f"M_M"] = vt.compute_mass_3(events,
+            particles[0],
+            particles[1],
+            particles[2],
+            events[f'{particles[0]}_mass'],
+            events[f'{particles[1]}_mass'],
+            events[f'{particles[2]}_mass'],)
+
+events[f"M_M_Kee"] = vt.compute_mass_3(events,
+            particles[0],
+            particles[1],
+            particles[2],
+            masses[321],
+            masses[11],
+            masses[11],)
+
+events[f"M_P"], events[f"M_PT"] = vt.compute_reconstructed_mother_momenta(events, 'M')
+events[f"missing_M_P"], events[f"missing_M_PT"] = vt.compute_missing_momentum(
+    events, 'M',particles
+)
+
+for particle_i in range(0, len(particles)):
+    (
+        events[f"delta_{particle_i}_P"],
+        events[f"delta_{particle_i}_PT"],
+    ) = vt.compute_reconstructed_momentum_residual(events, particles[particle_i])
+
+for m in ["m_01", "m_02", "m_12"]:
+    events[m] = events[m].fillna(0)
+
+################################################################################
+
+for particle in particles:
+    events[f"angle_{particle}"] = vt.compute_angle(events, 'M', f"{particle}")
+
+events["IP_B"] = vt.compute_impactParameter(events,'M',particles)
+for particle in particles:
+    events[f"IP_{particle}"] = vt.compute_impactParameter_i(events,'M', f"{particle}")
+events["FD_B"] = vt.compute_flightDistance(events,'M',particles)
+events["DIRA_B"] = vt.compute_DIRA(events,'M',particles)
+
+print(events)
+
+import uproot3
+
+def write_df_to_root(df, output_name):
+	branch_dict = {}
+	data_dict = {}
+	dtypes = df.dtypes
+	used_columns = [] # stop repeat columns, kpipi_correction was getting repeated
+	for dtype, branch in enumerate(df.keys()):
+		if branch not in used_columns:
+			if dtypes[dtype] == 'uint32': dtypes[dtype] = 'int32'
+			if dtypes[dtype] == 'uint64': dtypes[dtype] = 'int64'
+			branch_dict[branch] = dtypes[dtype]
+			# stop repeat columns, kpipi_correction was getting repeated
+			if np.shape(df[branch].shape)[0] > 1:
+				data_dict[branch] = df[branch].iloc[:, 0]
+			else:
+				data_dict[branch] = df[branch]
+		used_columns.append(branch)
+	with uproot3.recreate(output_name) as f:
+		f["DecayTree"] = uproot3.newtree(branch_dict)
+		f["DecayTree"].extend(data_dict)
+
+write_df_to_root(events, "/users/am13743/fast_vertexing_variables/datasets/B2KEE_three_body_cut_more_vars.root")
---
+++ src/fast_vertex_quality/tools/variables_tools.py
@@ -0,0 +1,346 @@
+from fast_vertex_quality.tools.config import read_definition, rd
+
+import fast_vertex_quality.tools.data_loader as data_loader
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import vector
+
+
+
+def compute_DIRA(df, mother, particles, true_vars=True):
+
+    PX = 0
+    PY = 0
+    PZ = 0
+
+    if true_vars:
+        for particle in particles:
+            PX += df[f"{particle}_TRUEP_X"]
+            PY += df[f"{particle}_TRUEP_Y"]
+            PZ += df[f"{particle}_TRUEP_Z"]
+    else:
+        for particle in particles:
+            PX += df[f"{particle}_PX"]
+            PY += df[f"{particle}_PY"]
+            PZ += df[f"{particle}_PZ"]
+
+    A = norm(np.asarray([
+        PX,
+        PY,
+        PZ]))
+
+    B = norm(
+        np.asarray(
+            [
+                df[f"{mother}_ENDVERTEX_X"] - df[f"{mother}_OWNPV_X"],
+                df[f"{mother}_ENDVERTEX_Y"] - df[f"{mother}_OWNPV_Y"],
+                df[f"{mother}_ENDVERTEX_Z"] - df[f"{mother}_OWNPV_Z"],
+            ]
+        )
+    )
+    dira = dot(A, B) / np.sqrt(mag(A) ** 2 * mag(B) ** 2)
+
+    return dira
+
+
+
+def compute_flightDistance(df, mother, particles, true_vars=True):
+
+    PX = 0
+    PY = 0
+    PZ = 0
+
+    if true_vars:
+        for particle in particles:
+            PX += df[f"{particle}_TRUEP_X"]
+            PY += df[f"{particle}_TRUEP_Y"]
+            PZ += df[f"{particle}_TRUEP_Z"]
+    else:
+        for particle in particles:
+            PX += df[f"{particle}_PX"]
+            PY += df[f"{particle}_PY"]
+            PZ += df[f"{particle}_PZ"]
+        
+    momenta = vector.obj(
+        px=PX,
+        py=PY,
+        pz=PZ,
+    )
+
+    end_vertex = np.asarray(
+        [df[f"{mother}_ENDVERTEX_X"], df[f"{mother}_ENDVERTEX_Y"], df[f"{mother}_ENDVERTEX_Z"]]
+    )
+    primary_vertex = np.asarray(
+        [df[f"{mother}_OWNPV_X"], df[f"{mother}_OWNPV_Y"], df[f"{mother}_OWNPV_Z"]]
+    )
+
+    momenta_array = np.asarray([momenta.px, momenta.py, momenta.pz])
+    P = momenta.mag
+    t = 0
+    for i in range(3):
+        t += momenta_array[i] / P * (primary_vertex[i] - end_vertex[i])
+    dist = 0
+    for i in range(3):
+        dist += (t * momenta_array[i] / P) ** 2
+    dist = np.sqrt(dist)
+
+    return dist
+
+
+def mag(vec):
+    sum_sqs = 0
+    for component in vec:
+        sum_sqs += component**2
+    mag = np.sqrt(sum_sqs)
+    return mag
+
+
+def norm(vec):
+    mag_vec = mag(vec)
+    for component_idx in range(np.shape(vec)[0]):
+        vec[component_idx] *= 1.0 / mag_vec
+    return vec
+
+
+def dot(vec1, vec2):
+    dot = 0
+    for component_idx in range(np.shape(vec1)[0]):
+        dot += vec1[component_idx] * vec2[component_idx]
+    return dot
+
+def compute_angle(df, mother, particle, true_vars=True):
+
+    if true_vars:
+        momenta_B = np.asarray(
+            [df[f"{mother}_TRUEP_X"], df[f"{mother}_TRUEP_Y"], df[f"{mother}_TRUEP_Z"]]
+        )
+        momenta_i = np.asarray(
+            [
+                df[f"{particle}_TRUEP_X"],
+                df[f"{particle}_TRUEP_Y"],
+                df[f"{particle}_TRUEP_Z"],
+            ]
+        )
+    else:
+        momenta_B = np.asarray([df[f"{mother}_PX"], df[f"{mother}_PY"], df[f"{mother}_PZ"]])
+        momenta_i = np.asarray(
+            [
+                df[f"{particle}_PX"],
+                df[f"{particle}_PY"],
+                df[f"{particle}_PZ"],
+            ]
+        )
+
+    dot_prod = np.arccos(dot(momenta_B, momenta_i) / (mag(momenta_B) * mag(momenta_i)))
+
+    dot_prod[np.where(np.isnan(dot_prod))] = 1e-6
+    dot_prod[np.where(dot_prod == 0)] = 1e-6
+
+    return dot_prod
+
+
+def compute_impactParameter_i(df, mother, particle, true_vars=True):
+
+    if true_vars:
+        momenta = vector.obj(
+            px=df[f"{particle}_TRUEP_X"],
+            py=df[f"{particle}_TRUEP_Y"],
+            pz=df[f"{particle}_TRUEP_Z"],
+        )
+    else:
+        momenta = vector.obj(
+            px=df[f"{particle}_PX"],
+            py=df[f"{particle}_PY"],
+            pz=df[f"{particle}_PZ"],
+        )
+    end_vertex = np.asarray(
+        [df[f"{mother}_ENDVERTEX_X"], df[f"{mother}_ENDVERTEX_Y"], df[f"{mother}_ENDVERTEX_Z"]]
+    )
+    primary_vertex = np.asarray(
+        [df[f"{mother}_OWNPV_X"], df[f"{mother}_OWNPV_Y"], df[f"{mother}_OWNPV_Z"]]
+    )
+
+    momenta_array = np.asarray([momenta.px, momenta.py, momenta.pz])
+    P = momenta.mag
+    t = 0
+    for i in range(3):
+        t += momenta_array[i] / P * (primary_vertex[i] - end_vertex[i])
+    dist = 0
+    for i in range(3):
+        dist += (primary_vertex[i] - end_vertex[i] - t * momenta_array[i] / P) ** 2
+    dist = np.sqrt(dist)
+
+    return dist
+
+
+
+def compute_impactParameter(df, mother, particles, true_vars=True):
+
+    PX = 0
+    PY = 0
+    PZ = 0
+
+    if true_vars:
+        for particle in particles:
+            PX += df[f"{particle}_TRUEP_X"]
+            PY += df[f"{particle}_TRUEP_Y"]
+            PZ += df[f"{particle}_TRUEP_Z"]
+    else:
+        for particle in particles:
+            PX += df[f"{particle}_PX"]
+            PY += df[f"{particle}_PY"]
+            PZ += df[f"{particle}_PZ"]
+        
+    momenta = vector.obj(
+        px=PX,
+        py=PY,
+        pz=PZ,
+    )
+
+    end_vertex = np.asarray(
+        [df[f"{mother}_ENDVERTEX_X"], df[f"{mother}_ENDVERTEX_Y"], df[f"{mother}_ENDVERTEX_Z"]]
+    )
+    primary_vertex = np.asarray(
+        [df[f"{mother}_OWNPV_X"], df[f"{mother}_OWNPV_Y"], df[f"{mother}_OWNPV_Z"]]
+    )
+
+    momenta_array = np.asarray([momenta.px, momenta.py, momenta.pz])
+    P = momenta.mag
+    t = 0
+    for i in range(3):
+        t += momenta_array[i] / P * (primary_vertex[i] - end_vertex[i])
+    dist = 0
+    for i in range(3):
+        dist += (primary_vertex[i] - end_vertex[i] - t * momenta_array[i] / P) ** 2
+    dist = np.sqrt(dist)
+
+    return dist
+
+
+
+
+
+
+# def compute_delta_mom(df, particle):
+def compute_reconstructed_momentum_residual(df, particle):
+
+    PX = df[f"{particle}_PX"] - df[f"{particle}_TRUEP_X"]
+    PY = df[f"{particle}_PY"] - df[f"{particle}_TRUEP_Y"]
+    PZ = df[f"{particle}_PZ"] - df[f"{particle}_TRUEP_Z"]
+
+    return np.sqrt((PX**2 + PY**2 + PZ**2)) * 1e-3, np.sqrt((PX**2 + PY**2)) * 1e-3
+
+
+# def compute_miss_mom(df, mother, particles, true_vars=True):
+def compute_missing_momentum(df, mother, particles, true_vars=True):
+
+    if true_vars:
+        PX = df[f"{mother}_TRUEP_X"]
+        PY = df[f"{mother}_TRUEP_Y"]
+        PZ = df[f"{mother}_TRUEP_Z"]
+
+        for particle in particles:
+            PX += -1.0 * df[f"{particle}_TRUEP_X"]
+            PY += -1.0 * df[f"{particle}_TRUEP_Y"]
+            PZ += -1.0 * df[f"{particle}_TRUEP_Z"]
+    else:
+        PX = df[f"{mother}_PX"]
+        PY = df[f"{mother}_PY"]
+        PZ = df[f"{mother}_PZ"]
+
+        for particle in particles:
+            PX += -1.0 * df[f"{particle}_PX"]
+            PY += -1.0 * df[f"{particle}_PY"]
+            PZ += -1.0 * df[f"{particle}_PZ"]
+
+    return np.sqrt((PX**2 + PY**2 + PZ**2)) * 1e-3, np.sqrt((PX**2 + PY**2)) * 1e-3
+
+
+# def compute_B_mom(df, mother, true_vars=True):
+def compute_reconstructed_mother_momenta(df, mother, true_vars=True):
+
+    if true_vars:
+        PX = df[f"{mother}_TRUEP_X"]
+        PY = df[f"{mother}_TRUEP_Y"]
+        PZ = df[f"{mother}_TRUEP_Z"]
+    else:
+        PX = df[f"{mother}_PX"]
+        PY = df[f"{mother}_PY"]
+        PZ = df[f"{mother}_PZ"]
+
+    return np.sqrt((PX**2 + PY**2 + PZ**2)) * 1e-3, np.sqrt((PX**2 + PY**2)) * 1e-3
+
+
+def compute_mass_3(df, i, j, k, mass_i, mass_j, mass_k, true_vars=True):
+
+    if true_vars:
+        PE = np.sqrt(
+            mass_i**2
+            + df[f"{i}_TRUEP_X"] ** 2
+            + df[f"{i}_TRUEP_Y"] ** 2
+            + df[f"{i}_TRUEP_Z"] ** 2
+        ) + np.sqrt(
+            mass_j**2
+            + df[f"{j}_TRUEP_X"] ** 2
+            + df[f"{j}_TRUEP_Y"] ** 2
+            + df[f"{j}_TRUEP_Z"] ** 2
+        ) + np.sqrt(
+            mass_k**2
+            + df[f"{k}_TRUEP_X"] ** 2
+            + df[f"{k}_TRUEP_Y"] ** 2
+            + df[f"{k}_TRUEP_Z"] ** 2
+        )
+        PX = df[f"{i}_TRUEP_X"] + df[f"{j}_TRUEP_X"] + df[f"{k}_TRUEP_X"]
+        PY = df[f"{i}_TRUEP_Y"] + df[f"{j}_TRUEP_Y"] + df[f"{k}_TRUEP_Y"]
+        PZ = df[f"{i}_TRUEP_Z"] + df[f"{j}_TRUEP_Z"] + df[f"{k}_TRUEP_Z"]
+    else:
+        PE = np.sqrt(
+            mass_i**2 + df[f"{i}_PX"] ** 2 + df[f"{i}_PY"] ** 2 + df[f"{i}_PZ"] ** 2
+        ) + np.sqrt(
+            mass_j**2 + df[f"{j}_PX"] ** 2 + df[f"{j}_PY"] ** 2 + df[f"{j}_PZ"] ** 2
+        ) + np.sqrt(
+            mass_k**2 + df[f"{k}_PX"] ** 2 + df[f"{k}_PY"] ** 2 + df[f"{k}_PZ"] ** 2
+        )
+        PX = df[f"{i}_PX"] + df[f"{j}_PX"] + df[f"{k}_PX"]
+        PY = df[f"{i}_PY"] + df[f"{j}_PY"] + df[f"{k}_PY"]
+        PZ = df[f"{i}_PZ"] + df[f"{j}_PZ"] + df[f"{k}_PZ"]
+
+    mass = np.sqrt((PE**2 - PX**2 - PY**2 - PZ**2) * 1e-6)
+
+    return mass
+
+
+
+# def compute_mass(df, i, j, mass_i, mass_j, true_vars=True):
+def compute_mass(df, i, j, mass_i, mass_j, true_vars=True):
+
+    if true_vars:
+        PE = np.sqrt(
+            mass_i**2
+            + df[f"{i}_TRUEP_X"] ** 2
+            + df[f"{i}_TRUEP_Y"] ** 2
+            + df[f"{i}_TRUEP_Z"] ** 2
+        ) + np.sqrt(
+            mass_j**2
+            + df[f"{j}_TRUEP_X"] ** 2
+            + df[f"{j}_TRUEP_Y"] ** 2
+            + df[f"{j}_TRUEP_Z"] ** 2
+        )
+        PX = df[f"{i}_TRUEP_X"] + df[f"{j}_TRUEP_X"]
+        PY = df[f"{i}_TRUEP_Y"] + df[f"{j}_TRUEP_Y"]
+        PZ = df[f"{i}_TRUEP_Z"] + df[f"{j}_TRUEP_Z"]
+    else:
+        PE = np.sqrt(
+            mass_i**2 + df[f"{i}_PX"] ** 2 + df[f"{i}_PY"] ** 2 + df[f"{i}_PZ"] ** 2
+        ) + np.sqrt(
+            mass_j**2 + df[f"{j}_PX"] ** 2 + df[f"{j}_PY"] ** 2 + df[f"{j}_PZ"] ** 2
+        )
+        PX = df[f"{i}_PX"] + df[f"{j}_PX"]
+        PY = df[f"{i}_PY"] + df[f"{j}_PY"]
+        PZ = df[f"{i}_PZ"] + df[f"{j}_PZ"]
+
+    mass = np.sqrt((PE**2 - PX**2 - PY**2 - PZ**2) * 1e-6)
+
+    return mass, (PE**2 - PX**2 - PY**2 - PZ**2)
