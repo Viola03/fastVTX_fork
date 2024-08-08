6c6e4d0444a73e2abfb6abf6442dcb1472de5e45
Alexander Mclean Marshall <alex.marshall@cern.ch>
2024-08-08 18:04:00 +0200

    davinci_general.py

---
+++ davinci/davinci_general.py
@@ -0,0 +1,431 @@
+IS_MC = True
+IS_EE = True
+IS_SS = False
+IS_EMU = False
+year = "2018"
+
+from CommonParticles.Utils import DefaultTrackingCuts
+from DecayTreeTuple.Configuration import *
+
+from GaudiKernel.SystemOfUnits import *
+from Gaudi.Configuration import *
+from Configurables import (
+    DecayTreeTuple,
+    TupleToolKinematic,
+)
+from Configurables import (
+    TupleToolPid,
+)
+
+from Configurables import DecayTreeTuple
+from Configurables import DaVinci
+
+import numpy as np
+
+
+
+negative_particles = [11, 13, -211, -321]
+positive_particles = [-11, -13, 211, 321]
+particle_dict = {
+11:"e-", -11:"e+",
+13:"mu-", -13:"mu+",
+-211:"pi-", 211:"pi+",
+-321:"K-", 321:"K+",
+}
+combinations = np.empty((0,3))
+for p1 in positive_particles:
+    for p2 in positive_particles:
+        for n1 in negative_particles:
+            combinations = np.append(combinations, [[p1,p2,n1]], axis=0)
+print(np.shape(combinations))
+# Sort each row to make the order of particles irrelevant
+sorted_combinations = np.sort(combinations, axis=1)
+# Extract unique rows
+unique_combinations = np.unique(sorted_combinations, axis=0)
+print(np.shape(unique_combinations))
+full_list_of_decays = []
+full_list_of_decays_string = '['
+for idx, combination in enumerate(unique_combinations):
+    # _Decays.append(f"[ B+ -> {particle_dict[combination[0]]} {particle_dict[combination[1]]} {particle_dict[combination[2]]} ]cc")
+    full_list_of_decays.append("[ B+ -> %s %s %s ]cc"%(particle_dict[combination[0]],particle_dict[combination[1]],particle_dict[combination[2]]))
+
+    if idx > 0:
+        full_list_of_decays_string += ", " 
+    full_list_of_decays_string += "(B+ -> %s %s %s)"%(particle_dict[combination[0]],particle_dict[combination[1]],particle_dict[combination[2]])
+full_list_of_decays_string += "]CC" 
+
+
+config_electron = {
+    "stripping_line": "Bu2LLK_eeLine2",
+    "decayname": "B2Kee",
+    # 'decay': "[B+ -> ^K+ ^mu+ ^mu-]CC",
+    # 'branches': {
+    #     "MOTHER": "[ B+ -> K+ mu+ mu-]CC",
+    #     "DAUGHTER1": "[ B+ ->  ^K+ mu+ mu-]CC",
+    #     "DAUGHTER2": "[B+ ->  K+ ^mu+ mu-]CC",
+    #     "DAUGHTER3": "[B+ ->  K+ mu+ ^mu-]CC"},
+
+    # 'decay': "[B+ -> ^K+ ^e+ ^e-]CC",
+    # 'branches': {
+    #     "MOTHER": "[ B+ -> K+ e+ e-]CC",
+    #     "DAUGHTER1": "[ B+ ->  ^K+ e+ e-]CC",
+    #     "DAUGHTER2": "[B+ ->  K+ ^e+ e-]CC",
+    #     "DAUGHTER3": "[B+ ->  K+ e+ ^e-]CC"},
+    
+    # 'decay': "[(B+ -> K+ e+ e-), (B- -> K+ mu+ mu-]",
+    # 'branches': {
+    #     "MOTHER": "[ B+ -> K+ e+ e-]CC",
+    #     "DAUGHTER1": "[ B+ ->  ^K+ e+ e-]CC",
+    #     "DAUGHTER2": "[B+ ->  K+ ^e+ e-]CC",
+    #     "DAUGHTER3": "[B+ ->  K+ e+ ^e-]CC"},
+
+    'decay': "[X+ -> ^X+ ^X+ ^X-]CC",
+    'branches': {
+        "MOTHER": "[ X+ -> X+ X+ X-]CC",
+        "DAUGHTER1": "[ X+ ->  ^X+ X+ X-]CC",
+        "DAUGHTER2": "[X+ ->  X+ ^X+ X-]CC",
+        "DAUGHTER3": "[X+ ->  X+ X+ ^X-]CC"},
+     
+    # # 'decay': "[B+ -> ^pi+ ^pi+ ^pi-]CC",
+    # 'branches': {
+    #     "MOTHER": "[ B+ -> pi+ pi+ pi-]CC",
+    #     "DAUGHTER1": "[ B+ ->  ^pi+ pi+ pi-]CC",
+    #     "DAUGHTER2": "[B+ ->  pi+ ^pi+ pi-]CC",
+    #     "DAUGHTER3": "[B+ ->  pi+ pi+ ^pi-]CC"},
+
+}
+
+
+# _Decays = ["[ B+ -> K+ e+ e- ]cc", "[ B+ -> K+ K+ K- ]cc"] 
+
+
+
+from StrippingConf.Configuration import StrippingConf, StrippingStream
+
+
+
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+
+__all__ = ("Bu2LLKConf", "default_config")
+
+default_config = {
+    "NAME": "Bu2LLK",
+    "CONFIG": {
+        "DaughterPT":250.0,
+        "UpperMass": 99999.0,
+        "BMassWindow":99999.0,
+        # "BMassWindow":50.0,
+        "BVertexCHI2": 9999.0,
+        "BIPCHI2": 9999.0,
+        "BDIRA": 0.0,
+        "BFlightCHI2": 0.0,
+        "DiLeptonPT": 0.0,
+    },
+}
+
+from Gaudi.Configuration import *
+from GaudiConfUtils.ConfigurableGenerators import (
+    FilterDesktop,
+    CombineParticles,
+)
+from PhysSelPython.Wrappers import (
+    Selection,
+    MergedSelection,
+)
+from StrippingConf.StrippingLine import StrippingLine
+from StrippingUtils.Utils import LineBuilder
+
+
+class GeneralConf(LineBuilder):
+    """
+    Builder for R_X measurements
+    """
+    __configuration_keys__ = (
+        "DaughterPT",
+        "UpperMass",
+        "BMassWindow",
+        "BVertexCHI2",
+        "BIPCHI2",
+        "BDIRA",
+        "BFlightCHI2",
+        "DiLeptonPT",
+    )
+
+    def __init__(self, name, config):
+        LineBuilder.__init__(self, name, config)
+
+        self._name = name
+
+        eeXLine_name = name + "_ee"
+
+        from StandardParticles import StdLooseElectrons as Electrons
+        from StandardParticles import StdLooseMuons as Muons
+        from StandardParticles import StdLoosePions as Pions
+        from StandardParticles import StdLooseKaons as Kaons
+
+        # from StandardParticles import StdAllNoPIDsElectrons as Electrons
+        # from StandardParticles import StdAllNoPIDsMuons as Muons
+        # from StandardParticles import StdAllNoPIDsPions as Pions
+        # from StandardParticles import StdAllNoPIDsKaons as Kaons
+
+        SelElectrons = self._filterHadron(
+            name="ElectronsFor" + self._name, sel=Electrons, params=config
+        )
+
+        SelMuons = self._filterHadron(
+            name="MuonsFor" + self._name, sel=Muons, params=config
+        )
+
+        SelKaons = self._filterHadron(
+            name="KaonsFor" + self._name, sel=Kaons, params=config
+        )
+
+        SelPions = self._filterHadron(
+            name="PionsFor" + self._name, sel=Pions, params=config
+        )
+
+        
+        SelB2eeXFromTracks = self._makeB2LLX(
+            eeXLine_name + "2",
+            hadrons=[
+                SelElectrons,
+                SelMuons,
+                SelPions,
+                SelKaons,
+            ],
+            params=config,
+            masscut="ADAMASS('B+') <  %(BMassWindow)s *MeV" % config,
+        )
+
+        self.B2eeXFromTracksLine = StrippingLine(
+            eeXLine_name + "Line2",
+            prescale=1,
+            postscale=1,
+            selection=SelB2eeXFromTracks,
+            RequiredRawEvents=[],
+            MDSTFlag=False,
+        )
+
+        self.registerLine(self.B2eeXFromTracksLine)
+
+
+    #####################################################
+    def _filterHadron(self, name, sel, params):
+        """
+        Filter for all hadronic final states
+        """
+        _Code = (
+            "(PT > %(DaughterPT)s *MeV)" % params
+        )
+
+        _Filter = FilterDesktop(Code=_Code)
+
+        return Selection(name, Algorithm=_Filter, RequiredSelections=[sel])
+
+    
+    # #####################################################
+    # def _filterDiLepton( self, name, dilepton, params ) :
+    #     """
+    #     Handy interface for dilepton filter
+    #     """
+
+    #     _Code = (
+    #         "(PT > %(DiLeptonPT)s *MeV)" % params
+    #     )
+
+    #     _Filter = FilterDesktop( Code = _Code )
+
+    #     return Selection(name, Algorithm = _Filter, RequiredSelections = [ dilepton ] )
+
+
+    #####################################################
+    def _makeB2LLX(
+        self, name, hadrons, params, masscut
+    ):
+        """
+        CombineParticles / Selection for the B
+        """
+
+        _Decays = ["[ B+ -> K+ e+ e- ]cc", "[ B+ -> K+ mu+ mu- ]cc", "[ B+ -> K+ K+ K- ]cc", "[ B+ -> pi+ pi+ pi- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+        # _Decays = ["[ B+ -> K+ e+ e- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+        # _Decays = ["[ B+ -> K+ K+ K- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+        
+        # _Decays = ["[ B+ -> pi+ e+ e- ]cc", "[ B+ -> pi+ mu+ mu- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+        _Decays = ["[ B+ -> K+ e+ e- ]cc", "[ B+ -> K+ mu+ mu- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+
+
+        # THIS MATCHED THE ABOVE - is it because StdAllNoPIDsKaons and StdAllNoPIDsPions have basically 100% overlap - DEBUNKED
+        # _Decays = ["[ B+ -> K+ e+ e- ]cc", "[ B+ -> K+ mu+ mu- ]cc", "[ B+ -> K+ K+ K- ]cc", "[ B+ -> pi+ pi+ pi- ]cc"] # PID cuts in StdLoose are wide enough to avoid all patterns and avoid repeated candidates?
+        _Decays = full_list_of_decays
+
+
+
+        # _Cut = (
+        #     "((VFASPF(VCHI2/VDOF) < %(BVertexCHI2)s) "
+        #     "& (BPVIPCHI2() < %(BIPCHI2)s) "
+        #     "& (BPVDIRA > %(BDIRA)s) "
+        #     # "& (ABSID > 0) "
+        #     "& ((mcMatch('B+')) | (mcMatch('B-')) | (mcMatch('B0')) | (mcMatch('B~0')) | (mcMatch('B_s0')) | (mcMatch('B_s~0'))  | (mcMatch('B_c+')) | (mcMatch('B_c-')))"
+        #     "& (BPVVDCHI2 > %(BFlightCHI2)s))" % params
+        # )
+
+        _Cut = (
+            "((mcMatch('B+')) | (mcMatch('B-')) | (mcMatch('B0')) | (mcMatch('B~0')) | (mcMatch('B_s0')) | (mcMatch('B_s~0'))  | (mcMatch('B_c+')) | (mcMatch('B_c-')))"
+        )
+
+        _Combine = CombineParticles(
+            Preambulo = ["from LoKiPhysMC.decorators import *","from LoKiPhysMC.functions import mcMatch"],
+            DecayDescriptors=_Decays, CombinationCut=masscut, MotherCut=_Cut
+        )
+
+        _Merge = MergedSelection("Merge" + name, RequiredSelections=hadrons)
+
+        return Selection(
+            name, Algorithm=_Combine, RequiredSelections=[_Merge]
+        )
+
+###############################################################################################################################################################
+###############################################################################################################################################################
+###############################################################################################################################################################
+
+
+builder = GeneralConf
+stripping_config = default_config
+
+mod_stripping_config = stripping_config["CONFIG"]
+
+builder_name = "Bu2LLK"
+# builder_name = "General"
+
+lb = builder(builder_name, mod_stripping_config)
+
+stream = StrippingStream("MyStream")
+
+cfg = config_electron
+
+for line in lb.lines():
+    print(line.name())
+    if line.name() == "Stripping" + cfg["stripping_line"]:
+        stream.appendLines([line])
+# quit()
+
+from Configurables import ProcStatusCheck
+
+bad_events_filter = ProcStatusCheck()
+
+sc = StrippingConf(
+    Streams=[stream],
+    MaxCandidates=4000,
+    AcceptBadEvents=False,
+    BadEventSelection=bad_events_filter,
+)
+
+
+#  | *AllStreams/Phys/StdAllNoPIDsElectrons/Particles                                                  |       47 |     2.0+-0.1    | 0.5     |    1 | 4      |
+#  | *AllStreams/Phys/StdAllNoPIDsKaons/Particles                                                      |      134 |     3.2+-0.3    | 2.0     |    1 | 9      |
+#  | *AllStreams/Phys/StdAllNoPIDsMuons/Particles                                                      |      102 |     2.5+-0.1    | 0.9     |    1 | 5      |
+#  | *AllStreams/Phys/StdAllNoPIDsPions/Particles                                                      |      174 |     3.9+-0.3    | 2.2     |    1 | 10     |
+#  | *AllStreams/Phys/StdAllNoPIDsProtons/Particles  
+
+
+# path_to_look = "Phys/" + cfg["stripping_line"] + "/Particles"
+# path_to_look = "pPhys/Particles"
+# path_to_look = "MC/Particles"
+
+from Configurables import DecayTreeTuple
+
+tuple = DecayTreeTuple(cfg["decayname"] + "_Tuple")
+# tuple.Inputs = [path_to_look]
+
+
+# tuple.Inputs = ["Phys/StdAllLooseMuons/Particles", "Phys/StdAllLooseKaons/Particles", "Phys/StdLooseDiElectron/Particles"]
+# tuple.Inputs = ["Phys/StdAllLooseMuons/Particles", "Phys/StdAllLooseKaons/Particles", "Phys/StdLoosePions/Particles", "Phys/StdLooseElectrons/Particles"]
+# tuple.Inputs = ["pPhys/Particles"]
+# tuple.Inputs = ["Phys/B2XMuMu_Line/Particles", "Phys/Bu2LLK_eeLine2/Particles"]
+# tuple.Inputs = ["Phys/B2XMuMu_Line/Particles"]
+# tuple.Inputs = ["Phys/General_eeLine2/Particles"]
+tuple.Inputs = ["Phys/Bu2LLK_eeLine2/Particles"]
+
+tuple.Decay = cfg["decay"]
+
+tuple.addBranches(cfg["branches"])
+
+tuple.ToolList += [
+    "TupleToolAngles",
+    "TupleToolEventInfo",
+    "TupleToolPrimaries",
+    "TupleToolPropertime",
+    "TupleToolTrackInfo",
+    # "TupleToolBremInfo",
+    "TupleToolRecoStats",
+    # "TupleToolMuonPid",
+    "TupleToolMCBackgroundInfo",
+    # "TupleToolL0Data",
+    # "TupleToolANNPID",
+]
+
+tuple.ToolList += ["TupleToolMCTruth"]
+tuple.ToolList += ["MCTupleToolEventType"]
+
+from Configurables import TupleToolMCTruth
+mc_truth = TupleToolMCTruth()
+mc_truth.ToolList = ["MCTupleToolKinematic", "MCTupleToolHierarchy"]
+tuple.addTool(mc_truth)
+tuple.TupleToolMCTruth.Verbose = True
+
+# MCTupleToolHierarchyOtherTracks
+# https://gitlab.cern.ch:8443/lhcb/Analysis/-/merge_requests/707/diffs#diff-content-279d6abf536ade7c3044e046262dba6dd103778c
+
+from Configurables import TupleToolKinematic
+
+tuple.ToolList += ["TupleToolKinematic"]
+tuple.addTool(TupleToolKinematic, name="TupleToolKinematic")
+tuple.TupleToolKinematic.Verbose = True
+
+
+from Configurables import DaVinci
+
+from Configurables import EventNodeKiller
+
+eventNodeKiller = EventNodeKiller("Stripkiller")
+eventNodeKiller.Nodes = ["/Event/AllStreams", "/Event/Strip"]
+
+DaVinci()
+# DaVinci().EvtMax = -1
+# DaVinci().PrintFreq = 1000
+# DaVinci().EvtMax = 200
+# DaVinci().EvtMax = 25
+# DaVinci().PrintFreq = 250
+DaVinci().EvtMax = 10
+DaVinci().PrintFreq = 1
+DaVinci().Simulation = IS_MC
+DaVinci().Lumi = not IS_MC
+if IS_MC:
+    DaVinci().appendToMainSequence([eventNodeKiller])
+    DaVinci().appendToMainSequence([sc.sequence()])
+DaVinci().UserAlgorithms = [tuple]
+DaVinci().VerboseMessages = True
+if year != "2018":
+    DaVinci().DataType = year
+else:
+    DaVinci().DataType = "2017"
+DaVinci().TupleFile = "DVntuple.root"
+
+print("Local INFO    DaVinci configurations")
+print("Local INFO    Data Type  = {}".format(DaVinci().DataType))
+print("Local INFO    Simulation = {}".format(DaVinci().Simulation))
+
+
+from GaudiConf import IOHelper
+IOHelper().inputFiles(
+    [
+        # "/eos/home-m/marshall/DL-Advocate2/Kee.dst"
+        # "/eos/home-m/marshall/DL-Advocate2/00140982_00000034_7.AllStreams.dst"
+        # "/afs/cern.ch/work/m/marshall/fast_vertexing_variables/davinci/00113947_00000003_7.AllStreams.dst"
+        # "/afs/cern.ch/work/m/marshall/fast_vertexing_variables/davinci/Kee.dst"
+        "/afs/cern.ch/work/m/marshall/fast_vertexing_variables/davinci/Kmumu.dst"
+    ],
+    clear=True,
+)
